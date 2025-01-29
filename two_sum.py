65bda4b7473ecfbbfd622a7e12fe2f1c7e793dd3
ICarlota99 <k431999@gmail.com>
2025-01-29 18:56:18 -0500

    two sum solved

--- two_sum.py
+++ two_sum.py
@@ -1,28 +1,22 @@
-def main():
-    nums = [2,7,11,15] 
-    target = 9
-    twoSum(nums, target)
-
-
-def twoSum(nums, target):
-    # Define an output list
-    numbers = []
-    # Order the input list in descending order
-    ordered_nums = []
-    ordered_nums.extend(nums)
-    ordered_nums.sort(reverse=True)
-
-    # Determine number of elements in list
-    size = len(nums)
-        
-    # Search for combinations in the input list
-    for i in range(size):
-        if ordered_nums[i] <= target:
-            second_number = target - ordered_nums[i]
-            if second_number in ordered_nums:
-                numbers.append(nums.index(ordered_nums[i]))
-                numbers.append(nums.index(second_number))
-                print(numbers.sort)
-                break
-
-main()
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Define an output list
+        numbers = []
+        # Order the input list in descending order
+        ordered_nums = []
+        ordered_nums.extend(nums)
+        ordered_nums.sort(reverse=True)
+            
+        # Search for combinations in the input list
+        for number in ordered_nums:
+            if number <= target:
+                second_number = target - number
+                if second_number == number:
+                    indexes = [i for i, num in enumerate(nums) if num == number]
+                    if len(indexes)==2:
+                        return(indexes)
+                elif second_number in ordered_nums:
+                    numbers.append(nums.index(number))
+                    numbers.append(nums.index(second_number))
+                    return(numbers)
+            pass
