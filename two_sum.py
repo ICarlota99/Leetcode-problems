941c1e6a3b6907900384a004ed2801885e6145fb
ICarlota99 <k431999@gmail.com>
2025-01-29 19:38:46 -0500

    # Time and Space complexity: O(n) # Choose this solution because search is faster

--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # Define an output list
@@ -9,14 +11,15 @@ class Solution:
             
         # Search for combinations in the input list
         for number in ordered_nums:
-            if number <= target:
-                second_number = target - number
-                if second_number == number:
-                    indexes = [i for i, num in enumerate(nums) if num == number]
-                    if len(indexes)==2:
-                        return(indexes)
-                elif second_number in ordered_nums:
-                    numbers.append(nums.index(number))
-                    numbers.append(nums.index(second_number))
-                    return(numbers)
-            pass
+            second_number = target - number
+            if second_number == number:
+                indexes = [i for i, num in enumerate(nums) if num == number]
+                if len(indexes)==2:
+                    return(indexes)
+            elif second_number in ordered_nums:
+                numbers.append(nums.index(number))
+                numbers.append(nums.index(second_number))
+                return(numbers)
+
+# Time and Space complexity: O(n)
+# Choose this solution because search is faster
