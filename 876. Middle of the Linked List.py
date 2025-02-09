fd399cadcc62b21e286f6349eaa1caf0d9a8bcbf
ICarlota99 <k431999@gmail.com>
2025-02-09 17:40:42 -0500

    solved(easy, two pinters, linked list, tortoise & hare): 876. Middle of the Linked List

---
+++ 876. Middle of the Linked List.py
@@ -0,0 +1,26 @@
+"""
+    Given the head of a singly linked list, return the middle node of the linked list.
+
+    If there are two middle nodes, return the second middle node.
+    Input: head = [1,2,3,4,5]
+    Output: [3,4,5]
+    Explanation: The middle node of the list is node 3.
+"""
+# Easy # Linked List # Two Pointers # tortoise and hare approach
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        output = []
+        slow = head # Middle node
+        fast = head # Last node
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
